5c48adbd125a132ccb609f9e5ee640d8204ea4ed
ksh-jinhap <ksh2574@jinhap.com>
2026-09-09 10:55:27 +0900

    Add files via upload

---
+++ config.js
@@ -0,0 +1,7 @@
+// Supabase 프로젝트 연결 정보
+// 연결 완료본
+// 주의: secret/service_role 키는 절대로 브라우저 코드에 넣지 마세요.
+window.APP_CONFIG = {
+  SUPABASE_URL: 'https://zeykximulnayuaklkoyk.supabase.co',
+  SUPABASE_ANON_KEY: 'sb_publishable_l01uL5y5UyLJuBx-O2OSUA_aeiakmXk'
+};
